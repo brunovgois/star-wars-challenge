98f191edafd46f79a89717a18e860167b8ebd428
bruno-gois <brunovgois@gmail.com>
2020-07-25 20:02:07 -0300

    Answer name of the first launched movie

--- src/index.js
+++ src/index.js
@@ -5,19 +5,24 @@ Parse.initialize(
   "Jl21MbJjOzHoq3eNjK0dY1cuRyQnOeu1GNIGQpY3",
   "Aqy19HFsorwxIhCC3E9IYBQh8WBnkdoMGOdEaYPe"
 );
+
 Parse.serverURL = "https://parseapi.back4app.com/";
 
 const app = express();
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  const Character = Parse.Object.extend("Character");
-  const query = new Parse.Query(Character);
+  const Film = Parse.Object.extend("Film");
+  const firstLaunchedMovie = new Parse.Query(Film);
+
+  firstLaunchedMovie.ascending("releaseDate")
 
   try {
-    const results = await query.find();
-    res.json(results);
-  } catch (e) {
+    const result = await firstLaunchedMovie.first();    
+    console.log(result.get("title"))
+    res.json()
+
+  } catch(e) {
     console.log(e);
   }
 });
