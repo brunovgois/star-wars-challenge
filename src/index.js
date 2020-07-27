89d518e2464d82affdb9f919b41ae3df5addaad9
bruno-gois <brunovgois@gmail.com>
2020-07-27 19:51:56 -0300

    Log answer of the third question

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const Parse = require("parse/node");
 const firstQuestion = require("./questions/firstQuestion");
 const secondQuestion = require("./questions/secondQuestion")
-
+const thirdQUestion = require("./questions/thirdQuestion")
 
 Parse.initialize(
   "Jl21MbJjOzHoq3eNjK0dY1cuRyQnOeu1GNIGQpY3",
@@ -12,3 +12,4 @@ Parse.serverURL = "https://parseapi.back4app.com/";
 
 new firstQuestion().answer(Parse);
 new secondQuestion().answer(Parse);
+new thirdQUestion().answer(Parse)
---
+++ src/questions/thirdQuestion.js
@@ -0,0 +1,24 @@
+module.exports = class thirdQuestion {
+  constructor() {}
+
+  //How many males and females characters exists
+  async answer(Parse) {
+    const Character = Parse.Object.extend("Character");
+    const charactersPerGenderQuery = new Parse.Query(Character);
+    let males = 0
+    let females = 0
+
+    try {
+      const results = await charactersPerGenderQuery.find();
+      for (let i = 0; i < results.length; i++) {
+        var object = results[i];
+        if (object.get("gender") == "male") males++;
+        else if (object.get("gender") == "female") females++;
+      }
+
+      console.log(males + " " + females);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+};
