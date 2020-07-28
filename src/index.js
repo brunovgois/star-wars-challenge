3d38e4bc26cb5a554e47ddc436b26f440da7c8bf
bruno-gois <brunovgois@gmail.com>
2020-07-28 02:58:45 -0300

    Log answer of the fifth question

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const Parse = require("parse/node");
 const firstQuestion = require("./questions/FirstQuestion");
 const secondQuestion = require("./questions/SecondQuestion");
 const thirdQuestion = require("./questions/ThirdQuestion");
-const fourthQuestion= require("./questions/FourthQuestion");
+const fourthQuestion = require("./questions/FourthQuestion");
+const fifthQuestion = require("./questions/FifthQuestion");
 
 Parse.initialize(
   "Jl21MbJjOzHoq3eNjK0dY1cuRyQnOeu1GNIGQpY3",
@@ -15,3 +16,4 @@ new firstQuestion().answer(Parse);
 new secondQuestion().answer(Parse);
 new thirdQuestion().answer(Parse);
 new fourthQuestion().answer(Parse);
+new fifthQuestion().answer(Parse);
---
+++ src/questions/FifthQuestion.js
@@ -0,0 +1,25 @@
+module.exports = class FifthQuestion {
+  constructor() {}
+
+  // How many characters speak the language "Gungan basic"?
+  async answer(Parse) {
+    const Character = Parse.Object.extend("Character");
+    const Specie = Parse.Object.extend("Specie");
+
+    const innerQuery = new Parse.Query(Specie);
+    innerQuery.equalTo("language", "Gungan basic");
+
+    const query = new Parse.Query(Character);
+    query.matchesQuery("species", innerQuery);
+    try {
+      const results = await query.find();
+  
+      for (let i = 0; i < results.length; i++) {
+        var object = results[i];
+        console.log(object.get("name"))
+      }
+    } catch(e) {
+      console.log(e);
+    }
+  }
+};
