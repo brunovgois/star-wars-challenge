3269fdc53920286bd653eca05a694d677b7caa08
bruno-gois <brunovgois@gmail.com>
2020-07-28 16:17:39 -0300

    wait all questions function to complete before loging the answer & create writeCsv function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const Parse = require("parse/node");
 const firstQuestion = require("./questions/FirstQuestion");
 const secondQuestion = require("./questions/SecondQuestion");
 const thirdQuestion = require("./questions/ThirdQuestion");
@@ -6,16 +5,70 @@ const fourthQuestion = require("./questions/FourthQuestion");
 const fifthQuestion = require("./questions/FifthQuestion");
 const sixthQuestion = require("./questions/SixthQuestion");
 
+const Parse = require("parse/node");
+const fs = require("fs");
+const csv = require("fast-csv");
+
+const answerArray = [];
+
 Parse.initialize(
   "Jl21MbJjOzHoq3eNjK0dY1cuRyQnOeu1GNIGQpY3",
   "Aqy19HFsorwxIhCC3E9IYBQh8WBnkdoMGOdEaYPe"
 );
-
 Parse.serverURL = "https://parseapi.back4app.com/";
 
-new firstQuestion().answer(Parse);
-new secondQuestion().answer(Parse);
-new thirdQuestion().answer(Parse);
-new fourthQuestion().answer(Parse);
-new fifthQuestion().answer(Parse);
-new sixthQuestion().answer(Parse);
+async function getAnswers() {
+  const result = await Promise.all([
+    new firstQuestion().answer(Parse),
+    new secondQuestion().answer(Parse),
+    new thirdQuestion().answer(Parse),
+    new fourthQuestion().answer(Parse),
+    new fifthQuestion().answer(Parse),
+    new sixthQuestion().answer(Parse),
+  ]);
+  return result;
+}
+
+(async () => {
+  const result = await getAnswers();
+  const [
+    firstAnswer,
+    secondAnswer,
+    thirdAnswer,
+    fourthAnswer,
+    fifthAnswer,
+    sixthAnswer,
+  ] = result;
+
+  console.log(result);
+  // writeCsv(firstAnswer, secondAnswer)
+})();
+
+function writeCsv(
+  firstAnswer,
+  secondAnswer,
+  thirdAnswer,
+  fourthAnswer,
+  fifthAnswer,
+  sixthAnswer
+) {
+  if (fs.existsSync("answers.csv")) {
+    fs.unlink("answers.csv", (e) => {
+      console.log(e);
+    });
+  }
+
+  const row = `${firstAnswer};${secondAnswer};${thirdAnswer}`;
+
+  const ws = fs.createWriteStream("answers.csv");
+  csv.parse({ delimiter: ";" });
+  csv
+    .write(
+      [
+        ["Pergunta 1;Pergunta 2;Pergunta 3;Pergunta 4;Pergunta 5;Pergunta 6"],
+        [row],
+      ],
+      { header: true }
+    )
+    .pipe(ws);
+}
